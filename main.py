e84b95ec79afc098ab2c540e91e562cff5971a4d
Stan Hill <hill.stan1738@gmail.com>
2025-08-03 23:27:21 +0100

    Revert "Initial model training setup"
    This reverts commit 43f9ef75527cbe0f62dcd7dc5d346d40f085ea68.

--- main.py
+++
@@ -1,8 +0,0 @@
-import optuna
-from train.objective import objective
-
-study = optuna.create_study(direction="minimize")
-study.optimize(objective, n_trials=20)
-
-print("Best trial:")
-print(study.best_trial)
